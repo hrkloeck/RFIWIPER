38937fbddc03b66fadb501a7521345dd9150612b
Claudia Comito <39374113+ClaudiaComito@users.noreply.github.com>
2024-06-13 11:45:17 +0200

    implement vectorized functions for Heat backend

--- RFI_LIB_SCANS.py
+++ RFI_LIB_SCANS.py
@@ -18,6 +18,11 @@ from copy import deepcopy
 from scipy.signal import convolve2d,correlate
 from scipy import stats
 import time
+# import heat from submodule
+import sys
+sys.path.append("heat/")
+import heat as ht
+
 
 def data_stats(data,stats_type='mean',accur=100):
     """
@@ -465,12 +470,13 @@ def flag_spec_by_smoothing(fg_spec,freq,cleanup_spec_mask,splitting,kernel_sizes
             grad_selectsp = flag_smoothing(sp_freq,sp_data,sp_data_mask,smooth_type=smooth_type[sp],kernel_sizes=kernel_sizes[sp],kernel_sequence_type=kernel_sequence_type[sp],\
                                                usedbinning=usedbinning[sp],bound_sigma=bound_sigma[sp],stats_type=stats_type[sp],\
                                                smooth_bound_kernel=smooth_bound_kernel[sp])
-
+            print("In flag_spec_by_smoothing: grad_selectsp, grad_select = ", grad_selectsp.shape, grad_select.shape)
             grad_select = np.append(grad_select,grad_selectsp)
 
 
         # clean up process is of the order of 0.04 sec
         #
+        print("In flag_spec_by_smoothing: grad_select = ", grad_select.shape)
         final_sp_mask = clean_up_1d_mask(grad_select,clean_bins,setvalue=True)
 
     else:
@@ -485,13 +491,112 @@ def flag_spec_by_smoothing(fg_spec,freq,cleanup_spec_mask,splitting,kernel_sizes
 
     return final_sp_mask
 
-def flag_spec_by_smoothing_vect(data,freq,mask,splitting,kernel_sizes,smooth_type,usedbinning,bound_sigma,stats_type,smooth_bound_kernel):
+def flag_spec_by_smoothing_ht(fg_spectra,freq,cleanup_spectra_mask,splitting,kernel_sizes,kernel_sequence_type,smooth_type,usedbinning,bound_sigma,stats_type,smooth_bound_kernel,clean_bins,idx=0):
     """
-    vectorized version of flag_spec_by_smoothing, operates on the whole dataset
-    """
-    pass
+    specially for doing  azimuth scans in batch
 
-def batch_convolve_1d_data(data,smooth_type='hanning',smooth_kernel=3):
+    Parameters
+    ----------
+    fg_spectra : ht.DNDarray
+        2D array of the spectra to be cleaned, shape (n, m) with n = timestamps, m = number of channels per timestamp
+    freq : np.ndarray or ht.DNDarray
+        1D array of the frequencies
+    cleanup_spectra_mask : ht.DNDarray
+        2D array of the mask for the spectra, shape (n, m) with n = timestamps, m = number of channels per timestamp
+    splitting : list
+        list of indices to split the spectrum into subarrays
+    kernel_sizes : list
+        list of kernel sizes for smoothing
+    smooth_type : list
+        list of smoothing types
+    usedbinning : list
+        list of binning sizes
+    bound_sigma : list
+        list of sigma values for boundary range
+    stats_type : list
+        list of statistics types
+    smooth_bound_kernel : list
+        list of kernel sizes for boundary smoothing
+    idx : int
+        index of the spectrum
+    """
+    from time import process_time
+
+    need_flagging = ht.sum(cleanup_spectra_mask, axis=1) != cleanup_spectra_mask.shape[1]
+    fg_spectra = fg_spectra[need_flagging]
+    cleanup_spectra_mask = cleanup_spectra_mask[need_flagging]
+    # allocate grad_select for all spectra
+    grad_select = ht.zeros(fg_spectra.shape, split=0, device=fg_spectra.device) 
+
+    for sp in range(len(splitting)-1):
+        if splitting[sp+1] == -1:
+            sp_slice = slice(splitting[sp], None)
+            # sp_freq = freq[splitting[sp]:]
+            # sp_data = fg_spectra[:, splitting[sp]:]
+            # sp_data_mask = cleanup_spectra_mask[splitting[sp]:]
+        else:
+            sp_slice = slice(splitting[sp], splitting[sp+1])
+            # sp_freq = freq[splitting[sp]:splitting[sp+1]]
+            # sp_data = fg_spectra[:, splitting[sp]:splitting[sp+1]]
+            # sp_data_mask = cleanup_spectra_mask[:, splitting[sp]:splitting[sp+1]]
+
+        sp_freq = freq[sp_slice]
+        sp_data = fg_spectra[:, sp_slice]
+        sp_data_mask = cleanup_spectra_mask[:, sp_slice]
+
+        if (len(sp_freq)%usedbinning[sp]) != 0:
+            print('\nCAUTION \n\nThe setting produced sub-arrays with no equal sizes.')
+            print('Subrray size is and used binning parameter ',len(sp_freq),usedbinning[sp])
+            print('Change usedbinning parameter in main programme\n\n')
+            sys.exit(-1)
+
+        grad_select[:, sp_slice] = flag_smoothing_ht(sp_freq, sp_data, sp_data_mask, smooth_type=smooth_type[sp], kernel_sizes=kernel_sizes[sp], kernel_sequence_type=kernel_sequence_type[sp],\
+                                            usedbinning=usedbinning[sp], bound_sigma=bound_sigma[sp], stats_type=stats_type[sp], smooth_bound_kernel=smooth_bound_kernel[sp])
+
+    final_spectra_mask = clean_up_1d_mask(grad_select, clean_bins, setvalue=True)
+    # if np.sum(cleanup_spectra_mask) != len(cleanup_spectra_mask):
+
+    #     # do the smooth flagging
+    #     #
+    #     grad_select = np.array([]).astype(bool)
+    #     for sp in range(len(splitting)-1):
+
+    #         if splitting[sp+1] == -1:
+    #             sp_freq       = freq[splitting[sp]:]
+    #             sp_data       = np.array(fg_spec)[splitting[sp]:]
+    #             sp_data_mask  = cleanup_spec_mask[splitting[sp]:]
+    #         else:
+    #             sp_freq       = freq[splitting[sp]:splitting[sp+1]]
+    #             sp_data       = np.array(fg_spec)[splitting[sp]:splitting[sp+1]]
+    #             sp_data_mask  = cleanup_spec_mask[splitting[sp]:splitting[sp+1]]
+
+
+    #         if (len(sp_freq)%usedbinning[sp]) != 0:
+    #             print('\nCAUTION \n\nThe setting produced sub-arrays with no equal sizes.')
+    #             print('Subrray size is and used binning parameter ',len(sp_freq),usedbinning[sp])
+    #             print('Change usedbinning parameter in main programme\n\n')
+    #             sys.exit(-1)
+
+
+    #         grad_selectsp = flag_smoothing(sp_freq,sp_data,sp_data_mask,smooth_type=smooth_type[sp],kernel_sizes=kernel_sizes[sp],kernel_sequence_type=kernel_sequence_type[sp],\
+    #                                            usedbinning=usedbinning[sp],bound_sigma=bound_sigma[sp],stats_type=stats_type[sp],\
+    #                                            smooth_bound_kernel=smooth_bound_kernel[sp])
+
+    #         grad_select = np.append(grad_select,grad_selectsp)
+
+
+    #     # clean up process is of the order of 0.04 sec
+    #     #
+    #     final_sp_mask = clean_up_1d_mask(grad_select,clean_bins,setvalue=True)
+
+    # else:
+
+    #     final_sp_mask = cleanup_spec_mask.astype(bool)
+
+    # return final_sp_mask
+
+
+def batch_convolve_1d_data(data,smooth_type='hamming',smooth_kernel=3):
     """
     Vectorized version of convolve_1d_data. Uses PyTorch batch convolutions instead of scipy convolve, and Heat as a backend for parallelization.
 
@@ -505,24 +610,17 @@ def batch_convolve_1d_data(data,smooth_type='hanning',smooth_kernel=3):
         size of the smoothing kernel. Default is 3    
     """
     import torch
-    import heat as ht
 
     if smooth_type == 'hamming':
-        sm_kernel = torch.hamming_window(smooth_kernel, dtype=torch.float64)
+        sm_kernel = torch.hamming_window(smooth_kernel, dtype=data.larray.dtype)
+        sm_kernel = ht.array(sm_kernel, device=data.device)
     elif smooth_type == 'wiener':
-        # TODO no idea what's going on here
-        sm_kernel = torch.ones(smooth_kernel)
+        raise NotImplementedError("Heat backend: Wiener smoothing not implemented yet")
     else:
         raise ValueError(f"Unknown smoothing type: {smooth_type}")
     
-    # convert data to torch tensor and add a batch dimension
-    data_2d = torch.tensor(data).unsqueeze(0)
-
-    # expand the smoothing kernel to match the shape of the data
-    batch_sm_kernel = sm_kernel.unsqueeze(0).unsqueeze(0).expand(data_2d.shape[0], 1, -1)
-
     # batch-convolve each timestamp with the smoothing kernel
-    sm_data = torch.nn.functional.conv1d(data_2d, batch_sm_kernel, padding=smooth_kernel//2, groups=data_2d.shape[0]) / sm_kernel.sum()
+    sm_data = ht.convolve(data, sm_kernel, mode='same') / sm_kernel.sum()
 
     return sm_data
 
@@ -636,6 +734,77 @@ def flag_smoothing(freq,spec,spec_mask,smooth_type='wiener',kernel_sizes=2,kerne
 
     return grad_select
 
+def flag_smoothing_ht(freq,spec,spec_mask,smooth_type='wiener',kernel_sizes=2,kernel_sequence_type='middle_fast',usedbinning=1,bound_sigma=4,stats_type='median',smooth_bound_kernel=31):
+    """
+    Flagging by smoothing the spectrum. Vectorized/batch version using Heat as a backend for parallelization.
+
+    Parameters
+    ----------
+    freq : np.ndarray or ht.DNDarray
+        1D array of the frequencies
+    spec : ht.DNDarray
+        2D array of the spectra to be cleaned, shape (n, m) with n = timestamps, m = number of channels per timestamp
+    spec_mask : ht.DNDarray
+        2D array of the mask for the spectra, shape (n, m) with n = timestamps, m = number of channels per timestamp
+    smooth_type : str
+        type of smoothing kernel. Options are 'hamming', 'wiener'. Default is 'wiener', which convolves the 1D data with Wiener filter of size `smooth_kernel`
+    kernel_sizes : int
+        number of kernel sizes to use for smoothing. Default is 2
+    kernel_sequence_type : str
+        type of kernel sequence. Options are 'middle_fast', 'fast', 'slow'. Default is 'middle_fast'
+    usedbinning : int
+        binning size to use for statistics calculation. Default is 1
+    bound_sigma : int
+        sigma value for boundary range. Default is 4
+    stats_type : str
+        type of statistics to use for boundary range. Options are 'mean', 'madstd', 'madmedian', 'median'. Default is 'median'
+    smooth_bound_kernel : int
+        size of the smoothing kernel for boundary range. Default is 31
+    """
+    import matplotlib.pyplot as plt
+    from copy import copy
+
+    from time import process_time
+
+    grad_select     = copy(spec_mask)
+    grad_select_org = copy(spec_mask)
+
+
+    # set the kernel sequence
+    #
+    kernel = kernel_sequence(kernel_sizes,kernel_sequence_type)
+
+
+    info_fg   = []
+    info_fg_k = []
+
+    print("In flag_smoothing: len(kernel) = ", len(kernel), type(kernel))
+    print("In flag_smoothing: kernel = ", kernel)
+    for k in kernel:
+        
+        grad_select_org = grad_select
+
+        # smooth the original spectrum and subtract it from the
+        # original dataset
+        #
+        #start = time.perf_counter()
+        sm_data   = batch_convolve_1d_data(spec,smooth_type=smooth_type,smooth_kernel=k)
+        #end = time.perf_counter()
+        #print(f"In flag_smoothing: convolve_1d_data : {end - start:0.7f} seconds")
+        resi_data = spec - sm_data
+
+        # generate a flag based on the boundary region
+        #
+        #start = time.perf_counter()
+        grad_select,boundary_up,boundary_low = boundary_range(freq,resi_data,usedbinning,np.invert(grad_select),\
+                                                                  bound_sigma=bound_sigma,stats_type=stats_type,\
+                                                                  smooth_kernel=smooth_bound_kernel)
+        #end = time.perf_counter()
+        #print(f"In flag_smoothing: boundary_range : {end - start:0.7f} seconds")
+        # and combine with the previous                                                          
+        grad_select = np.logical_or(grad_select,grad_select_org)
+
+    return grad_select
 
 def clean_up_1d_mask(mask,clean_bins=[[1,0,1]],setvalue=1):
     """
@@ -670,13 +839,24 @@ def clean_up_1d_mask(mask,clean_bins=[[1,0,1]],setvalue=1):
     return inputmask.astype(bool)
 
 
-def clean_up_1d_maskold(mask,bins=[[True,False,True]],setvalue=True):
+def clean_up_1d_mask_ht(mask,bins=[[True,False,True]],setvalue=True):
     """
-    just clean up single entries, this takes for ages! sliding window mask matching!
+    vectorized sliding window mask matching
+
+    Parameters
+    ----------
+    mask : ht.DNDarray
+        2D array of the mask to be cleaned, shape (n, m) with n = timestamps, m = number of channels per timestamp
+    bins : list
+        list of masks to be used for cleaning
+    setvalue : bool
+        value to set the mask to. Default is True
     """
     from copy import deepcopy
     inputmask = deepcopy(mask)
 
+    timestamps = inputmask.shape[0]
+    channels = inputmask.shape[1]
     for k in range(len(bins)):
         # # original 
         # for i in range(len(inputmask)-len(bins[k])):
@@ -687,19 +867,23 @@ def clean_up_1d_maskold(mask,bins=[[True,False,True]],setvalue=True):
         
 
         # --> vectorized search and smaller loops (140x speedup):
-        # create a 2D version of inputmask with `len(inputmask)-len(bins[k])` rows and `len(bins[k])` columns
-        # Example: if inputmask were [0, 1, 2, 3, 4, 5], and len(bins[k]) = 3, then
+        # create a 3D version of inputmask with `len(inputmask)-len(bins[k])` rows and `len(bins[k])` columns for each timestamp
+        # Example: if inputmask[0] were [0, 1, 2, 3, 4, 5], and len(bins[k]) = 3, then
         # inputmask_2d = [[0, 1, 2], [1, 2, 3], [2, 3, 4], [3, 4, 5]]
-        inputmask_2d = np.zeros((len(inputmask)-len(bins[k]), len(bins[k])), dtype=bool)
+        shape_3d = (timestamps, channels-len(bins[k]), len(bins[k]))
+        inputmask_3d = ht.zeros(shape_3d, dtype=ht.bool, split=0, device=inputmask.device)
         for j in range(len(bins[k])):
-            inputmask_2d[:, j] = inputmask[j:len(inputmask)-len(bins[k])+j]
+            # local torch operation to speed up the process
+            # NB `larray` is the process-local array of the DNDarray (a torch tensor)
+            # TODO: replace this loop with ht.unfold once merged
+            inputmask_3d[:, :, j].larray = inputmask.larray[:, j:channels-len(bins[k])+j]
         # find the rows where all elements are equal to the elements in bins[k]            
-        isequal_2d = np.where((inputmask_2d == bins[k]).sum(axis=1) == len(bins[k]))
+        isequal_3d = ht.where((inputmask_3d == bins[k]).sum(axis=-1) == len(bins[k]))
         # set the values in original inputmask to setvalue
-        for i in isequal_2d[0]:
-            inputmask[i:i+len(bins[k])-1] = setvalue
+        for i in isequal_3d[0]:
+            inputmask[:, i:i+len(bins[k])-1] = setvalue
         # free up memory            
-        del inputmask_2d
+        del inputmask_3d
         
     return inputmask
 
